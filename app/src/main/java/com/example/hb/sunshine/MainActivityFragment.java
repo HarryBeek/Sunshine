879ac7d0752e3ff702413cd58123d4e313f81008
Ganondox <harrybeek90@hotmail.com>
2015-10-13 22:31:11 +0200

    Lesson 1 almost completed

--- app/src/main/java/com/example/hb/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/hb/sunshine/MainActivityFragment.java
@@ -5,11 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> mForecastAdapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +26,65 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, true);
+
+        String [] forecastArray = {
+                "Today - Sunny - 22C",
+                "Today - Sunny - 22C",
+                "Today - Sunny - 22C",
+                "Today - Sunny - 22C",
+                "Today - Sunny - 22C",
+                "Today - Sunny - 22C",
+        };
+
+        List<String> weekForecast = new ArrayList<String>(
+                Arrays.asList(forecastArray));
+
+        mForecastAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(),
+                        R.layout.list_item_forecast,
+                        R.id.list_item_forecast_textview,
+                        weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(
+                R.id.listview_forecast
+        );
+        listView.setAdapter(mForecastAdapter);
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
